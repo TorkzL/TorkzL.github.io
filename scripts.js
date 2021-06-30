d631ce91da42f2722a8d9d75b740c18aa44427a0
TorkzL <72087920+TorkzL@users.noreply.github.com>
2021-06-30 10:49:34 -0300

    Primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+alert('Olá, seja bem-vindo')
+
+let txt = "";
+
+ txt += "Cookies Enabled: " + navigator.cookieEnabled + "\n";
+ txt += "Browser Language: " + navigator.language + "\n";
+ txt += "Browser Online: " + navigator.onLine + "\n";
+ txt += "Platform: " + navigator.platform + "\n";
